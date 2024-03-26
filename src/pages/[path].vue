acf4e3423f3b2cc606c00c4c05dbc632cf862bce
xingwanyue <xwymail@qq.com>
2024-03-26 17:06:19 +0800

    blog detail

---
+++ src/pages/[path].vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { api } from '@/utils';
+import _ from 'lodash';
+const route = useRoute();
+
+const { data: article } = (await useFetch(`${api}/common/article?path=${route.params.path}`, {
+  server: true,
+})) as any;
+
+useSeoMeta({
+  title: () => article.value?.title,
+  description: () => article.value?.description,
+  keywords: () => article.value?.keywords,
+});
+</script>
+<template>
+  <div class="article">
+    <p class="articleContent" v-html="article.content"></p>
+  </div>
+</template>
+<style scoped lang="scss">
+.article {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .articleContent {
+    max-width: 1200px;
+    width: 100%;
+  }
+}
+</style>
