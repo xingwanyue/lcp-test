3056aeba6b73f639dc470679a034f6695aa92982
xingwanyue <xwymail@qq.com>
2024-03-27 09:53:02 +0800

    test

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,4 +15,10 @@ const localePath = useLocalePath();
 </script>
 <template>
   <NuxtLink :to="localePath('home')">Go Home</NuxtLink>
+  <div
+    id="g_id_onload"
+    data-client_id="1044858520955-9ua24gpj8m98avtbp030t6dp624fi689.apps.googleusercontent.com"
+    data-login_uri="https://app.detpractice.com/weapp/api/common/login"
+    data-auto_select
+  ></div>
 </template>
