5eeca0e041d1c54d941b15302e39f4e2a0539e4e
xingcongcong <xingcongcong@henanyuexue.com>
2024-04-12 10:22:20 +0800

    login page

---
+++ src/pages/login.vue
@@ -0,0 +1,321 @@
+<script setup lang="ts">
+// 使用no-header-footer布局 不影响其他页面
+// definePageMeta({
+//   layout: false,
+// });
+
+import { ref, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useStore } from '@/store';
+import { oauth2SignIn } from '@/utils/googleAuth';
+import { getToken, saveToken } from '@/utils';
+// import { login } from '@/api';
+
+import googleImg from '../public/img/login/google_logo.svg';
+import lookImg from '../public/img/login/look.svg';
+import unlookImg from '../public/img/login/unlook.svg';
+import errIcon from '../public/img/login/errIcon.svg';
+
+const route = useRoute();
+const router = useRouter();
+const store = useStore();
+
+const url = route.query.url ? decodeURIComponent(route.query.url) : '/home';
+const formData = ref({});
+const loading = ref(false);
+const pwdShow = ref(false);
+const errMessage = ref('');
+const errShow = ref(false);
+
+onMounted(async () => {
+  const token = await getToken();
+  if (token) {
+    await store.dispatch('app/getUserInfo');
+    router.push(url);
+  }
+});
+
+const submit = async () => {
+  if (loading.value) return false;
+  const { email, password } = formData.value || {};
+  errShow.value = false;
+  if (!email) {
+    errShow.value = true;
+    errMessage.value = 'Please enter your email address.';
+    return false;
+  }
+  const pan = /\w+[@][a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)+/;
+  if (!pan.test(email)) {
+    errShow.value = true;
+    errMessage.value = 'Invalid email.';
+    return false;
+  }
+  if (!password) {
+    errShow.value = true;
+    errMessage.value = 'Please enter your password.';
+    return false;
+  }
+  loading.value = true;
+  const { err, data: { token } = {} } = await login({
+    email: formData.value.email,
+    password: formData.value.password,
+  });
+  if (!err) {
+    await saveToken(token, true);
+    await store.dispatch('app/getUserInfo');
+    router.push(url);
+  } else {
+    errShow.value = true;
+    errMessage.value = err.message;
+  }
+  loading.value = false;
+};
+
+const goForget = () => {
+  router.push('/forget');
+};
+
+const goRegister = () => {
+  router.push('/register');
+};
+
+const googleLogin = async () => {
+  return oauth2SignIn();
+};
+</script>
+
+<template>
+  <div class="login">
+    <div class="left">
+      <div class="t1">Welcome back</div>
+      <div class="t2">Login to practice</div>
+      <div class="loginGoogle" @click="googleLogin">
+        <img :src="googleImg" alt="" />
+        <span style="margin-left: 16px">Log in with Google</span>
+      </div>
+      <el-divider class="fengeline"><span>Or continue with</span></el-divider>
+      <el-form
+        ref="ruleFormRef"
+        :rules="rules"
+        :model="formData"
+        size="default"
+        class="login-form"
+        @submit.native.prevent
+      >
+        <el-form-item prop="email" label="">
+          Email address
+          <el-input v-model="formData.email" placeholder="you@example.com"> </el-input>
+        </el-form-item>
+        <el-form-item prop="password" label="">
+          Password
+          <el-input v-model="formData.password" :type="pwdShow ? 'text' : 'password'" placeholder="Password">
+            <template #suffix>
+              <img :src="pwdShow ? lookImg : unlookImg" alt="" @click="pwdShow = !pwdShow" />
+            </template>
+          </el-input>
+        </el-form-item>
+        <el-form-item style="margin-top: 24px; margin-bottom: 16px">
+          <div v-if="errShow" class="err-message">
+            <img :src="errIcon" class="errIcon" alt="" />
+            <span>{{ errMessage }}</span>
+          </div>
+          <div>
+            <el-button v-loading="loading" type="primary" native-type="submit" class="submit" @click="submit">
+              Login
+            </el-button>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <div class="zhuce">
+            <div class="goforget">
+              <span style="cursor: pointer" @click="goForget">Forgot password?</span>
+            </div>
+            <div class="goregister">
+              Don't have an account?
+              <span style="color: #f66442; cursor: pointer" @click="goRegister">Sign up here</span>
+            </div>
+          </div>
+        </el-form-item>
+      </el-form>
+    </div>
+    <!-- <div class="right">1</div> -->
+  </div>
+</template>
+
+<style lang="scss">
+.login {
+  .submit {
+    width: 400px;
+    border-radius: 25px;
+    height: 50px;
+    color: #fff;
+    background-color: #f66442;
+    border-color: #f66442;
+    font-size: 18px;
+
+    &:hover,
+    &:active {
+      color: #fff !important;
+      background-color: darken(#f66442, 10%) !important;
+    }
+  }
+  .el-input__inner {
+    // 设置光标颜色
+    caret-color: rgba(0, 0, 0, 0.45) !important;
+    &:hover,
+    &:active,
+    &:focus {
+      border-color: rgba(0, 0, 0, 0.45) !important;
+    }
+  }
+  .el-input__wrapper {
+    height: 40px;
+  }
+  //   .el-input__suffix-inner > img {
+  //     margin-top: 10px;
+  //   }
+}
+</style>
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f7f8f9;
+  //   overflow: hidden;
+  .left {
+    // border: 1px red solid;
+    flex: none;
+    width: 400px;
+
+    background-color: #f7f8f9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    // max-height: 960px;
+    .t1 {
+      width: 400px;
+      font-size: 32px;
+      font-family: PingFangSC, PingFang SC;
+      font-weight: 600;
+      color: #333333;
+      line-height: 45px;
+    }
+    .t2 {
+      width: 400px;
+      font-size: 16px;
+      font-family: PingFangSC, PingFang SC;
+      font-weight: 600;
+      color: rgba(0, 0, 0, 0.45);
+      line-height: 22px;
+      margin-top: 8px;
+    }
+    .err-message {
+      display: flex;
+      align-items: center;
+      width: 400px;
+      height: 26px;
+      background: #fef7f5;
+      border-radius: 4px;
+      border: 1px solid rgba(246, 100, 66, 0.1);
+      box-sizing: border-box;
+      color: #f66442;
+      font-size: 12px;
+      margin-bottom: 8px;
+      .errIcon {
+        width: 16px;
+        height: 16px;
+        margin-right: 8px;
+        margin-left: 8px;
+      }
+    }
+    .loginGoogle {
+      width: 400px;
+      height: 50px;
+      border-radius: 25px;
+      border: 1px solid #201515;
+      background: #ffffff;
+      font-size: 18px;
+      margin-top: 49px;
+      font-family: PingFangSC, PingFang SC;
+      font-weight: 600;
+      color: #333333;
+      cursor: pointer;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .fengeline {
+      width: 400px;
+      margin-top: 48px;
+      :deep(.el-divider__text) {
+        background-color: #f7f8f9 !important;
+        font-weight: 400;
+        font-size: 14px;
+        color: rgba(0, 0, 0, 0.45);
+      }
+    }
+
+    .goforget {
+      color: #f66442;
+      // cursor: pointer;
+    }
+  }
+  .right {
+    width: 50%;
+    flex: none;
+    background: #f66442;
+    position: relative;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    .loginImg {
+      width: calc(100% - 40px);
+      display: block;
+      // margin: auto;
+      // flex: none;
+    }
+  }
+}
+@media screen and (max-width: 958px) {
+  .login {
+    .right {
+      display: none;
+    }
+    .left {
+      height: 100vh;
+    }
+  }
+}
+@media screen and (max-width: 600px) {
+  .login {
+    .left {
+      width: calc(100% - 48px);
+      .t1 {
+        width: calc(100% - 48px);
+      }
+      .t2 {
+        width: calc(100% - 48px);
+      }
+      .err-message {
+        width: calc(100% - 48px);
+      }
+      .loginGoogle {
+        width: calc(100% - 48px);
+      }
+      .fengeline {
+        width: calc(100% - 48px);
+        margin-top: 48px;
+      }
+    }
+    .login-form {
+      width: calc(100% - 48px);
+    }
+    .submit {
+      width: 100%;
+    }
+  }
+}
+</style>
---
+++ src/utils/googleAuth.js
@@ -0,0 +1,65 @@
+// import { post } from './request';
+
+const YOUR_CLIENT_ID = '1044858520955-9ua24gpj8m98avtbp030t6dp624fi689.apps.googleusercontent.com';
+// secret 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
+const YOUR_REDIRECT_URI = 'https://app.detpractice.com/oauth';
+
+/*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+export function oauth2SignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create element to open OAuth 2.0 endpoint in new window.
+  const form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  const params = {
+    client_id: YOUR_CLIENT_ID,
+    redirect_uri: YOUR_REDIRECT_URI,
+    scope: 'https://www.googleapis.com/auth/userinfo.profile email',
+    state: 'try_sample_request',
+    include_granted_scopes: 'true',
+    response_type: 'token',
+  };
+
+  // Add form parameters as hidden input values.
+  Object.keys(params).forEach((p) => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  });
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
+
+// If there's an access token, try an API request.
+// Otherwise, start OAuth 2.0 flow.
+export const oauthLogin = async () => {
+  const hashString = window.location.hash.substring(1);
+  let accessToken = '';
+  hashString.split('&').forEach((str) => {
+    const [k, v] = str.split('=');
+    if (k === 'access_token') {
+      accessToken = v;
+    }
+  });
+  if (accessToken) {
+    const res = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const data = await res.json();
+    const { email, picture, name } = data;
+    // return post('/common/login', { email, avatar: picture, nickname: name, google: true, type: 'pc' });
+  }
+};
+export default {};
