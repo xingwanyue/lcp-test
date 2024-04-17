f28eeeb557067f363f3c27e044ef96ff36a3a9ec
xingwanyue <xwymail@qq.com>
2024-04-17 16:01:16 +0800

    api

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,8 @@ const host = 'https://app.detpractice.com/app/#/';
 const cdn = 'https://detcdn.zixuekeji.cn';
 // export const api = 'https://app.detpractice.com/weapp/api';
 // export const api = 'http://192.168.1.22:9000/api';
-export const api = 'http://192.168.1.147:11001/weapp/api';
+// export const api = 'http://192.168.1.147:11001/weapp/api';
+export const api = 'https://app.xingwanyue.com/weapp/api';
 
 export const urlGet = (url: string) => `${host}?url=${encodeURIComponent(url)}`;
 export const staticPcUrlGet = (path: string) => `${cdn}/store/pc/${path}`;
