acda0ff615ed3ce6d997f909617fab119af66ec0
xingcongcong <xingcongcong@henanyuexue.com>
2024-04-22 09:57:04 +0800

    图片路径

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -164,7 +164,7 @@ const googleLogin = () => {
               x5-video-orientation="portraint"
               x5-video-player-type="h5"
               src="/img/video_banner.mp4"
-              poster="/img/home/video_banner.png"
+              poster="/img/video_banner.png"
             ></video>
           </div>
         </div>
