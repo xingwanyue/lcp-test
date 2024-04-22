0cb26447278ecb271b4ea14ac4e0125bf300dfc4
xingcongcong <xingcongcong@henanyuexue.com>
2024-04-22 19:15:25 +0800

    ui

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -48,26 +48,37 @@ const handleCurrentChange = async (val: number) => {
     </div>
     <div class="bolgs_content_wrapper" style="">
       <div v-if="blogs && blogs.length" class="bolgs_content">
-        <div v-for="(item, index) in blogs" :key="index" class="one_blog" data-aos="fade-up" data-aos-duration="1000">
-          <NuxtLink :to="localePath(`/${item.path}`)">
-            <div class="title">
-              {{ item.name }}
+        <NuxtLink
+          v-for="(item, index) in blogs"
+          :key="index"
+          class="one_blog"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          :to="localePath(`/${item.path}`)"
+        >
+          <div class="title">
+            {{ item.name }}
+          </div>
+          <div class="content">
+            {{ item.content }}
+          </div>
+          <div class="bottom">
+            <div class="date">{{ dayjs(item.createTime).format("YYYY-MM-DD") }}</div>
+            <div class="right_arrow">
+              <el-image src="/img/blog/right_arrow.svg"></el-image>
             </div>
-            <div class="content">
-              {{ item.content }}
-            </div>
-            <div class="bottom">
-              <div class="date">{{ dayjs(item.createTime).format("YYYY-MM-DD") }}</div>
-              <div class="right_arrow">
-                <el-image src="/img/blog/right_arrow.svg"></el-image>
-              </div>
-            </div>
-          </NuxtLink>
-        </div>
+          </div>
+        </NuxtLink>
       </div>
       <div class="pagination_out">
-        <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"
-          @current-change="handleCurrentChange" class="mt-4" />
+        <el-pagination
+          background
+          layout="prev, pager, next"
+          :total="total"
+          :page-size="pageSize"
+          @current-change="handleCurrentChange"
+          class="mt-4"
+        />
       </div>
     </div>
     <v-embark />
@@ -117,6 +128,7 @@ const handleCurrentChange = async (val: number) => {
       .one_blog {
         padding: 24px;
         border-radius: 8px;
+        padding-bottom:48px;
         border: 1px solid #e9e9e9;
         cursor: pointer;
 
@@ -135,21 +147,18 @@ const handleCurrentChange = async (val: number) => {
           font-weight: 600;
           font-size: 28px;
           color: #201515;
-          // &:hover {
-          //   color: #f66442;
-          // }
         }
         .content {
           font-weight: 400;
           font-size: 20px;
           color: #201515;
           margin-top: 24px;
-          height: 76px;
-          overflow: hidden;
           text-overflow: ellipsis;
           display: -webkit-box;
           -webkit-line-clamp: 3;
           -webkit-box-orient: vertical;
+          overflow: hidden;
+          line-height: 28px;
         }
         .bottom {
           margin-top: 24px;
@@ -157,10 +166,12 @@ const handleCurrentChange = async (val: number) => {
           justify-content: space-between;
           align-items: center;
           height: 24px;
-          width: calc(100% - 48px);
-          position: absolute;
-          left: 24px;
-          bottom: 24px;
+          position:absolute;
+          bottom:12px;
+          // border:1px red solid;
+          width:calc(100% - 48px);
+
+         
           .date {
             font-weight: 400;
             font-size: 16px;
