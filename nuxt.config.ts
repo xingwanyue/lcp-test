9eace54c139ae2ce6d228e6b337db2673c51dd3f
xingwanyue <xwymail@qq.com>
2024-04-29 15:08:54 +0800

    删除默认desc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
           async: true,
         },
       ],
-      title: 'DetPractice',
+      title: 'DET Practice:The best Duolingo English Test Practice platform',
       meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     },
   },
