9570c3ed932fc8c5dd2c1a2ce8b958a5f62452ec
xingwanyue <xwymail@qq.com>
2024-06-27 15:34:48 +0800

    多语言

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,5 +1,9 @@
 import en from '@/locales/en.json';
 import id from '@/locales/id.json';
+import fr from '@/locales/fr.json';
+import ja from '@/locales/ja.json';
+import pt from '@/locales/pt.json';
+import de from '@/locales/de.json';
 
 export default defineI18nConfig(() => ({
   legacy: false,
@@ -9,5 +13,9 @@ export default defineI18nConfig(() => ({
   messages: {
     en,
     id,
+    fr,
+    ja,
+    pt,
+    de,
   },
 }));
