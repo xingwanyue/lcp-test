ef7939fd1699ddee4c7005e0356b1be7c051297b
xingcongcong <xingcongcong@henanyuexue.com>
2024-06-28 15:29:05 +0800

    弹框添加所有配置语言

--- src/layouts/changeLanguage_TK.vue
+++ src/layouts/changeLanguage_TK.vue
@@ -8,102 +8,93 @@ const state = reactive({
   tkshow: false,
 });
 const wehaveLanguage = {
+  ar: 'العربية',
+  da: 'Dansk',
   de: 'Deutsch',
-  pt: 'Português',
-  ja: '日本語',
+  en: 'English',
+  es: 'Español',
   fr: 'Français',
   id: 'Bahasa Indonesia',
-  es: 'Español',
   it: 'Italiano',
-  th: 'ไทย',
-  pl: 'Polski',
+  ja: '日本語',
   ko: '한국어',
-  ar: 'العربية',
-  nl: 'Nederlands',
-  tr: 'Türkçe',
   nb: 'Norsk',
-  da: 'Dansk',
+  nl: 'Nederlands',
+  pl: 'Polski',
+  pt: 'Português',
   ru: 'Русский',
+  th: 'ไทย',
+  tr: 'Türkçe',
+  zh: '中文',
 };
 const changeLanguageTips = {
-  cn: {
-    area: 'Deutsch',
-    tips: '欢迎访问我们的网站,你可以切换到[中文]版本,以便更好地了解和使用我们的服务。',
+  ar: {
+    tips: 'مرحبًا بك في موقعنا. يمكنك التبديل إلى النسخة [العربية] لفهم واستخدام خدماتنا بشكل أفضل.',
+  },
+  da: {
+    tips: 'Velkommen til vores hjemmeside. Du kan skifte til [Dansk] versionen for at forstå og bruge vores tjenester bedre.',
   },
   de: {
-    area: 'Deutsch',
     tips: 'Willkommen auf unserer Website. Sie können auf die [Deutsch] Version wechseln, um unsere Dienste besser zu verstehen und zu nutzen.',
   },
-  pt: {
-    area: 'Português',
-    tips: 'Bem-vindo ao nosso site. Você pode mudar para a versão [Português] para entender e usar melhor nossos serviços.',
+  en: {
+    tips: 'Welcome to our website. You can switch to the [English] version to better understand and use our services.',
   },
-  ja: {
-    area: '日本語',
-    tips: '当社のウェブサイトへようこそ。[日本語]バージョンに切り替えると、サービスをより理解して利用できます。',
+  es: {
+    tips: 'Bienvenido a nuestro sitio web. Puede cambiar a la versión [Español] para comprender y utilizar mejor nuestros servicios.',
   },
   fr: {
-    area: 'Français',
     tips: 'Bienvenue sur notre site. Vous pouvez passer à la version [Français] pour mieux comprendre et utiliser nos services.',
   },
   id: {
-    area: 'Bahasa Indonesia',
     tips: 'Selamat datang di situs web kami. Anda dapat beralih ke versi [Bahasa Indonesia] untuk lebih memahami dan menggunakan layanan kami.',
   },
-  es: {
-    area: 'Español',
-    tips: 'Bienvenido a nuestro sitio web. Puede cambiar a la versión [Español] para comprender y utilizar mejor nuestros servicios.',
-  },
   it: {
-    area: 'Italiano',
     tips: 'Benvenuti nel nostro sito. Puoi passare alla versione [Italiano] per comprendere e utilizzare meglio i nostri servizi.',
   },
-  th: {
-    area: 'ไทย',
-    tips: 'ยินดีต้อนรับสู่เว็บไซต์ของเรา คุณสามารถเปลี่ยนเป็นเวอร์ชั่น [ไทย] เพื่อเข้าใจและใช้บริการของเราได้ดีขึ้น',
-  },
-  pl: {
-    area: 'Polski',
-    tips: 'Witaj na naszej stronie. Możesz przełączyć się na wersję [Polski], aby lepiej zrozumieć i korzystać z naszych usług.',
+  ja: {
+    tips: '当社のウェブサイトへようこそ。[日本語]バージョンに切り替えると、サービスをより理解して利用できます。',
   },
   ko: {
-    area: '한국어',
     tips: '당사 웹 사이트에 오신 것을 환영합니다. [한국어] 버전으로 전환하여 서비스를 더 잘 이해하고 활용할 수 있습니다.',
   },
-  ar: {
-    area: 'العربية',
-    tips: 'مرحبًا بك في موقعنا. يمكنك التبديل إلى النسخة [العربية] لفهم واستخدام خدماتنا بشكل أفضل.',
-  },
-  nl: {
-    area: 'Nederlands',
-    tips: 'Welkom op onze website. U kunt overschakelen naar de [Nederlands] versie om onze diensten beter te begrijpen en te gebruiken.',
-  },
-  tr: {
-    area: 'Türkçe',
-    tips: 'Web sitemize hoş geldiniz. Hizmetlerimizi daha iyi anlamak ve kullanmak için [Türkçe] sürümüne geçebilirsiniz.',
-  },
   nb: {
-    area: 'Norsk',
     tips: 'Velkommen til nettstedet vårt. Du kan bytte til [Norsk] -versjonen for å forstå og bruke tjenestene våre bedre.',
   },
-  da: {
-    area: 'Dansk',
-    tips: 'Velkommen til vores hjemmeside. Du kan skifte til [Dansk] versionen for at forstå og bruge vores tjenester bedre.',
+  nl: {
+    tips: 'Welkom op onze website. U kunt overschakelen naar de [Nederlands] versie om onze diensten beter te begrijpen en te gebruiken.',
+  },
+  pl: {
+    tips: 'Witaj na naszej stronie. Możesz przełączyć się na wersję [Polski], aby lepiej zrozumieć i korzystać z naszych usług.',
+  },
+  pt: {
+    tips: 'Bem-vindo ao nosso site. Você pode mudar para a versão [Português] para entender e usar melhor nossos serviços.',
   },
   ru: {
-    area: 'Русский',
     tips: 'Добро пожаловать на наш сайт. Вы можете переключиться на [Русский] версию, чтобы лучше понимать и использовать наши услуги.',
+  },
+  th: {
+    tips: 'ยินดีต้อนรับสู่เว็บไซต์ของเรา คุณสามารถเปลี่ยนเป็นเวอร์ชั่น [ไทย] เพื่อเข้าใจและใช้บริการของเราได้ดีขึ้น',
+  },
+  tr: {
+    tips: 'Web sitemize hoş geldiniz. Hizmetlerimizi daha iyi anlamak ve kullanmak için [Türkçe] sürümüne geçebilirsiniz.',
+  },
+  zh: {
+    tips: '欢迎访问我们的网站,你可以切换到[中文]版本,以便更好地了解和使用我们的服务。',
   },
 };
 const changeLanguage = (newVal) => {
   setLocale(newVal);
   state.tkshow = false;
 };
+
 if (process.client) {
-  state.language = navigator.language;
+  state.language = navigator.language.slice(0, 2).toLowerCase() || 'en';
+  console.log(navigator.language);
+  console.log(wehaveLanguage[state.language]);
   //   如果浏览器语言不是英语 且我们有这个语言 就显示弹框
-  if (navigator.language !== 'en') {
-    if (wehaveLanguage[navigator.language]) {
+  if (state.language !== 'en') {
+    if (wehaveLanguage[state.language]) {
       state.tkshow = true;
     }
   }
@@ -112,7 +103,7 @@ if (process.client) {
   //     changeLanguage('en');
   //   }
   //   如果浏览器语言跟i18n切换后的的语言一样，取消上面的逻辑,不显示弹框
-  if (locale.value === navigator.language) {
+  if (locale.value === state.language) {
     state.tkshow = false;
   }
 }
