fe0063067dde365f71976401015ae30aa89e709d
xingcongcong <xingcongcong@henanyuexue.com>
2024-07-03 17:56:55 +0800

    ui

--- src/pages/affiliate.vue
+++ src/pages/affiliate.vue
@@ -31,14 +31,81 @@ const contaceUsList = ref([
     btn: 'Check Out',
   },
 ]);
+const dosList = ref([
+  {
+    id: '1',
+    font: 'Try out Detpractice.com for yourself',
+  },
+  {
+    id: '2',
+    font: 'Promote Detpractice.com ethically and honestly',
+  },
+  { id: '3', font: 'Create quality and relevant content' },
+  { id: '4', font: 'Disclose your affiliate relationship' },
+]);
+const dontsList = ref([
+  {
+    id: '1',
+    font: 'Make misleading or false claims about Detpractice.com',
+  },
+  {
+    id: '2',
+    font: 'Promote on platforms that have inappropriate or illegal content',
+  },
+  {
+    id: '3',
+    font: 'Promote on any coupon site',
+  },
+  {
+    id: '4',
+    font: "Set up ad campaigns using our brand name, or putting our domain name in your ads' destination URLs",
+  },
+]);
+const faqList = ref([
+  {
+    id: '1',
+    q: "Who can apply for DET PracticeI's affiliate program?",
+    a: 'If you are a creator that has an audience composed of students,a training institution that teaches Duolingo English Test,or an influencer,you can sign up for our affiliate program and earn money.',
+  },
+  {
+    id: '2',
+    q: 'How do I track my commissions?',
+    a: "Once you've signed up for our affiliate program, you'll have access to your dedicated admin page where you can track conversions from your referral links.",
+  },
+  {
+    id: '3',
+    q: 'Do I get a commission if a customer I refer subscribes or upgrades?',
+    a: 'Yes, after a user signs up through your link, you earn a commission every time the user makes a purchase and upgrades.',
+  },
+  {
+    id: '4',
+    q: 'What is the exact percentage of commission I can earn?',
+    a: 'For the first 3 orders, you will receive 10% commission; for more than 3 orders, you will receive 30% commission.',
+  },
+  {
+    id: '5',
+    q: 'What if a customer I referred cancels their purchase?',
+    a: 'If a customer cancels a purchase or gets a refund for any reason, the commission you earned for that person will be deducted from your account.',
+  },
+  {
+    id: '6',
+    q: 'When and how do I get paid?',
+    a: 'We process payments on the 15th of each month for all commissions you have earned in the previous month. You can receive payments via PayPal.',
+  },
+  {
+    id: '7',
+    q: 'What if I have other questions about your affiliate internet marketing program?',
+    a: 'Just contact us at "<span class="yellow_font">support@detpractice.com</span>" and we\'ll be happy to help you with any questions you may have.',
+  },
+]);
 </script>
 <template>
   <div class="aff_out">
     <div class="aff_header">
-      <div class="aff_header_content" :style="`background-image: url(/img/aboutus/banner-aboutus.svg);`">
+      <div class="aff_header_content" :style="`background-image: url(/img/affiliate/30off.svg);`">
         <div class="tag">Affiliate Program</div>
         <h1 class="title">Earn <span class="yellow_font">30% Commission</span> From Your Every Referral</h1>
-        <div class="sing_up_now common_btn_hover_bgColor">Sign Up Now</div>
+        <NuxtLink class="sing_up_now common_btn_hover_bgColor">Sign Up Now</NuxtLink>
       </div>
     </div>
     <div class="content">
@@ -57,19 +124,27 @@ const contaceUsList = ref([
             <div class="earth_with_title">Earn With Detpractice's Diverse Product Range</div>
             <div class="earth_with_content">
               <div class="one_block one_block_blue">
-                <div class="one_block_icon"></div>
+                <div class="one_block_icon">
+                  <img src="/img/home/product_icon1.svg" :alt="$t('pricing.pagefont.alt')" />
+                </div>
                 <div class="one_block_title">Question bank</div>
               </div>
               <div class="one_block one_block_red">
-                <div class="one_block_icon"></div>
+                <div class="one_block_icon">
+                  <img src="/img/home/product_icon2.svg" :alt="$t('pricing.pagefont.alt')" />
+                </div>
                 <div class="one_block_title">AI Correction Service</div>
               </div>
               <div class="one_block one_block_yellow">
-                <div class="one_block_icon"></div>
+                <div class="one_block_icon">
+                  <img src="/img/home/product_icon3.svg" :alt="$t('pricing.pagefont.alt')" />
+                </div>
                 <div class="one_block_title">Mock Test</div>
               </div>
               <div class="one_block one_block_green">
-                <div class="one_block_icon"></div>
+                <div class="one_block_icon">
+                  <img src="/img/home/product_icon4.svg" :alt="$t('pricing.pagefont.alt')" />
+                </div>
                 <div class="one_block_title">Courses</div>
               </div>
             </div>
@@ -137,6 +212,45 @@ const contaceUsList = ref([
           </div>
         </div>
       </div>
+      <div class="dos_dom_out_wrapper">
+        <div class="dos_dom_out">
+          <div class="do_dom">
+            <div class="title">Do’s</div>
+            <div v-for="item in dosList" :key="item.id" class="one_tips">
+              <div class="icon">
+                <img src="/img/affiliate/green_right_icon.svg" :alt="$t('pricing.pagefont.alt')" />
+              </div>
+              <div class="font">{{ item.font }}</div>
+            </div>
+          </div>
+          <div class="do_dom">
+            <div class="title title_red">Don’ts</div>
+            <div v-for="item in dosList" :key="item.id" class="one_tips">
+              <div class="icon">
+                <img src="/img/affiliate/red_err_icon.svg" :alt="$t('pricing.pagefont.alt')" />
+              </div>
+              <div class="font">{{ item.font }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="aff_faq_out">
+        <div class="aff_faq">
+          <div class="title">FAQs</div>
+          <div class="aff_faq_article_out">
+            <div v-for="item in faqList" :key="item.id" class="one_faq">
+              <div class="one_faq_question">{{ item.q }}</div>
+              <div class="one_faq_answer" v-html="item.a"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="start_earn_out_wrapper">
+        <div class="start_earn_out">
+          <div class="title">Start Earning with Our Affiliate Program Now</div>
+          <NuxtLink class="btn common_btn_hover_bgColor">Sign Up Now</NuxtLink>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -154,7 +268,7 @@ const contaceUsList = ref([
       background-repeat: no-repeat;
       background-size: cover;
       background-position: center;
-      border: 1px red solid;
+
       // margin-top: 28px;
       @media (max-width: 906px) {
         padding: 100px 0;
@@ -213,6 +327,7 @@ const contaceUsList = ref([
         margin: 0 auto;
         margin-top: 40px;
         cursor: pointer;
+        display: block;
       }
     }
   }
@@ -271,7 +386,11 @@ const contaceUsList = ref([
               .one_block_icon {
                 width: 48px;
                 height: 48px;
-                border: 1px red solid;
+
+                img {
+                  width: 100%;
+                  height: 100%;
+                }
               }
               .one_block_title {
                 font-weight: 500;
@@ -321,21 +440,22 @@ const contaceUsList = ref([
     }
     .become_out_wrapper {
       padding: 0px 30px;
+      background: #fff4f1;
+      margin-top: 100px;
       @media screen and (max-width: 450px) {
         padding: 0px 15px;
       }
-      background: #fff4f1;
-      margin-top: 100px;
+
       @media (max-width: 450px) {
         margin-top: 50px;
       }
       .part3 {
         padding: 100px 0;
+        max-width: 1200px;
+        margin: 0 auto;
         @media (max-width: 450px) {
           padding: 50px 0;
         }
-        max-width: 1200px;
-        margin: 0 auto;
 
         .title {
           font-weight: 500;
@@ -466,6 +586,120 @@ const contaceUsList = ref([
               'imgarea'
               'fontarea';
           }
+        }
+      }
+    }
+    .dos_dom_out {
+    }
+    .dos_dom_out_wrapper {
+      padding: 0px 30px;
+      background: #f2f4f6;
+      .dos_dom_out {
+        padding: 120px 0;
+        max-width: 1200px;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+        grid-gap: 24px;
+        .do_dom {
+          padding: 40px;
+          background: #ffffff;
+          border-radius: 8px;
+          .title {
+            font-weight: 600;
+            font-size: 40px;
+            text-align: center;
+            margin-bottom: 40px;
+            color: #06cc76;
+          }
+          .title_red {
+            color: #f66442;
+          }
+          .one_tips {
+            display: flex;
+            justify-content: flex-start;
+            align-items: flex-start;
+            grid-gap: 16px;
+            margin-bottom: 16px;
+            .icon {
+              width: 18px;
+              height: 18px;
+            }
+            .font {
+              font-weight: 400;
+              font-size: 18px;
+              color: #403f3e;
+            }
+          }
+        }
+      }
+    }
+    .aff_faq_out {
+      margin: auto;
+      box-sizing: border-box;
+      max-width: 1260px;
+      padding: 0px 30px;
+      .aff_faq {
+        padding: 120px 0;
+        .title {
+          font-weight: 600;
+          font-size: 40px;
+          color: #201515;
+          text-align: center;
+          margin-bottom: 54px;
+        }
+        .aff_faq_article_out {
+          display: grid;
+          grid-gap: 24px 80px;
+          grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+          .one_faq {
+            padding: 23px 0px;
+            border-bottom: 1px solid #e9e9e9;
+            .one_faq_question {
+              font-weight: 600;
+              font-size: 20px;
+              color: #201515;
+              line-height: 28px;
+            }
+            .one_faq_answer {
+              font-weight: 400;
+              font-size: 18px;
+              color: #201515;
+              line-height: 26px;
+              margin-top: 16px;
+              ::v-deep(.yellow_font) {
+                color: #f66442;
+              }
+            }
+          }
+        }
+      }
+    }
+    .start_earn_out_wrapper {
+      padding: 0px 30px;
+      background: #fff4f1;
+      .start_earn_out {
+        padding: 80px 0;
+        max-width: 1200px;
+        margin: 0 auto;
+        .title {
+          font-weight: 600;
+          font-size: 48px;
+          color: #201515;
+          text-align: center;
+        }
+        .btn {
+          padding: 9px 24px;
+          background-color: #f66442;
+          border-radius: 22px;
+          font-weight: 500;
+          font-size: 18px;
+          color: #ffffff;
+          width: fit-content;
+          margin: 0 auto;
+          margin-top: 40px;
+          display: block;
+          cursor: pointer;
         }
       }
     }
