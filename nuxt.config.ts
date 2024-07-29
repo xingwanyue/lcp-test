2ab761c074de1e2ed9b659b643f78cab896e7f72
xingwanyue <xwymail@qq.com>
2024-07-29 17:22:13 +0800

    font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: '/css/carousel.css' },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css?family=Nunito',
+          href: '/css/font.css',
         },
       ],
       htmlAttrs: {
