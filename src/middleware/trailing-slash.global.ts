97dd99190e62c7d1045aee8342f51487e1521590
xingcongcong <xingcongcong@henanyuexue.com>
2024-11-13 18:15:48 +0800

    页面重定向到 /blog/path/pagesize （原来的/learn,/blog? ）

--- src/middleware/trailing-slash.global.ts
+++ src/middleware/trailing-slash.global.ts
@@ -2,4 +2,12 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (to.path.endsWith('/') && to.path !== '/') {
     return navigateTo({ path: to.path.substring(0, to.path.length - 1), query: to.query });
   }
+  // 原来的learn页面重定向到blog页面 参数为 /blog/category  category要查一下
+  if (to.path === '/learn') {
+    return navigateTo({ path: `/blog` });
+  }
+  // 原来的blog页面 路由带问号的重定向到不带问号的新路径
+  if (to.path === '/blog' && to.query.page) {
+    return navigateTo({ path: `/blog` });
+  }
 });
