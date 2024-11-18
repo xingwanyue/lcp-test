78a06bdb7d8646a603d4f08cad08161ef50a94d9
xingcongcong <xingcongcong@henanyuexue.com>
2024-11-18 16:28:16 +0800

    文案

--- src/pages/tos.vue
+++ src/pages/tos.vue
@@ -6,8 +6,8 @@ import { domain } from '@/utils';
 const state = reactive({});
 useSeoMeta({
   title: t('tos.seometa.title'),
-  // description: t('tos.seometa.description'),
-  // keywords: t('tos.seometa.keywords'),
+  description: t('tos.seometa.description'),
+  keywords: t('tos.seometa.keywords'),
 });
 
 const localePath = useLocalePath();
