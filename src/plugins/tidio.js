cb9757a09e43c965fb233e6a6ce52d148c6da8bf
xingwanyue <xwymail@qq.com>
2024-11-20 17:23:59 +0800

    answer save

--- src/plugins/tidio.js
+++ src/plugins/tidio.js
@@ -4,6 +4,7 @@ export default defineNuxtPlugin(() => {
     const s = d.createElement('script');
 
     s.src = 'https://code.tidio.co/wruqp7llixvkwsdlbkcqpid2jhxwl0cx.js';
+    s.defer = true;
     document.body.appendChild(s);
   }, 1000);
 });
